d57a2a783e93aef7e75d779fea94cdd1d0f2e7a1
adothsieh <alexhsieh98@gmail.com>
2019-06-14 21:55:45 -0700

    Packages and App files

--- frontend/greenspace-frontend/src/App.js
+++ frontend/greenspace-frontend/src/App.js
@@ -1,25 +1,32 @@
 import React from 'react';
-import logo from './logo.svg';
+import sakura from './images/icons/sakura.jpg';
+import logo from './images/icons/greenspace_logo.png';
 import './App.css';
+import {NavBar} from './components/NavBar';
+import {Footer} from './components/Footer';
+import {Photo} from './components/Photo';
+import {PhotoGallery} from './components/PhotoGallery';
+import {Questionnaire} from './components/Questionnaire';
+import {Map} from './components/Map';
+import {BrowserRouter, Route, Switch} from 'react-router-dom';
+
 
 function App() {
+  console.log(photoArray);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    
+      <BrowserRouter>
+        <NavBar />
+        <br></br>
+        <Switch>
+          <Route path="/" component={Map} exact/>
+          <Route path="/photogallery" component={() => <PhotoGallery photos={photoArray} />} />
+          <Route path="/questionnaire" component={Questionnaire} />
+          <Route path="/map" component={Map} />
+        </Switch>
+      </BrowserRouter>
+      
+    
   );
 }
 
