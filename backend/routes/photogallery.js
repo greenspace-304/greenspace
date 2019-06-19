7a7c9818d1cb3f2c3d28c05e6a075d4fc341bc54
John Sastrillo <john.sastrillo@gmail.com>
2019-06-19 12:16:01 -0700

    add fetch call to upload photo and route

--- backend/routes/photogallery.js
+++ backend/routes/photogallery.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var multer = require('multer');
 var dbCreds = require('./shared/credentials');
 var router = express.Router();
 
@@ -136,7 +137,71 @@ router.post('/remove_photo', function(req, res, next){
       res.send(Json.stringify(req.body))
     }
   })
-})
+});
+
+const maxFileSize = 1024*1024*20;
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb){
+    cb(null, './../frontend/greenspace-frontend/src/images/icons');
+  }
+  ,
+  filename: function (req, file, cb){
+    cb(null, file.originalname);
+  }
+});
+
+const fileFilter = (req, file, cb) => {
+  if(file.mimetype === 'image/jpeg' || file.mimetype === 'image/png'){
+    console.log("RIGHT FILE TYPE");
+    cb(null, true);
+  } else {
+    console.log("SHOULDNT HAVE REACHED HERE")
+    cb(null, false);
+  }
+};
+
+const upload = multer({
+  storage: storage,
+  limits: {
+    fileSize: maxFileSize
+  },
+  fileFilter: fileFilter
+});
+
+router.post('/upload_photo', upload.single('imageData'), function(req, res, next){
+      console.log(req.body);
+      console.log(req.file);
+
+      var mysql = require('mysql')
+      var connection = mysql.createConnection({
+       host: 'localhost',
+       user: dbCreds.dbUsername,
+       password: dbCreds.dbPassword,
+       database: 'greenspace'
+      })
+      connection.connect()
+
+      let insertPhoto = `insert into userphotos set?`;
+      let photoName = `${Date.now()}-${req.body.photoName}`
+      let insertUserPhotoParams =
+      {
+        caption: req.body.caption,
+        photoname: photoName,
+        userid: req.body.userId,
+        plantid: req.body.plantId,
+        photopath:  `../images/icons/${photoName}`,
+      }
+      connection.query(insertPhoto, insertUserPhotoParams, function (err, rows, fields) {
+        if (err) {
+          console.log(err);
+        } else {
+          res.header("Access-Control-Allow-Origin", "*");
+          res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+          res.send(Json.stringify(req.body))
+        }
+      })
+  })
 
 
 
--- frontend/greenspace-frontend/src/components/PhotoGallery.js
+++ frontend/greenspace-frontend/src/components/PhotoGallery.js
@@ -5,7 +5,6 @@ import {Photo} from './Photo';
 export class PhotoGallery extends React.Component {
     constructor(props){
         super(props);
-
         this.generatePhotos = this.generatePhotos.bind(this);
     }
 
@@ -13,12 +12,39 @@ export class PhotoGallery extends React.Component {
 
         const height = this.props.height;
         const width = this.props.width;
-        
+
         return this.props.photos.map(function (photo){
             console.log(photo.caption);
             console.log(photo.photo);
             return <div class="photo"><Photo photo={photo.photo} caption={photo.caption} height={height} width={width}/></div>
         });
+    }
+
+    uploadPhotos = (e) => {
+      let imageForm = new FormData();
+
+      imageForm.append("photoName", /**/);
+      imageForm.append("userId", /**/);
+      imageForm.append("plantId", /**/);
+      imageForm.append("caption", /**/);
+      imageForm.append("imageData", e.target.files[0])
+
+
+      let request =
+      { method: 'POST',
+        mode: 'cors',
+        body: imageForm,
+      };
+
+      fetch('http://localhost:9000/photo/upload_photo', request)
+      .then( (response) => {
+          console.log(response.body);
+          response.json();
+      })
+      .then( resp => {
+        console.log("Successful Upload")
+        })
+      .catch((error) => console.error(error));
     }
 
     render() {
@@ -27,8 +53,7 @@ export class PhotoGallery extends React.Component {
                 <h1>Photo Gallery</h1>
                 <div class="uploadPhoto">
                   <form method="post" enctype="multipart/form-data">
-                    <input type="file" name="files[]" multiple />
-                    <input type="submit" value="Upload File" name="submit" />
+                    <input type="file" onChange={(e) => this.uploadPhotos(e)} sname="files[]" multiple />
                   </form>
                 </div>
                 <div class="photoGallery">
